dc7cb7b989c7fb47fa79a2c281903efc77daf9d3
tarun1219 <41871545+tarun1219@users.noreply.github.com>
2025-04-11 11:19:23 -0700

    Fixed Inventory Page Refresh Issue

--- src/pages/InventoryPage.jsx
+++ src/pages/InventoryPage.jsx
@@ -3,14 +3,6 @@ import DataUploader from "../components/Inventory/DataUploader";
 import IndexNavbar from "../components/Navbars/IndexNavbar";
 
 const InventoryPage = () => {
-  useEffect(() => {
-    if (!window.location.search.includes("refreshed=true")) {
-      const separator = window.location.href.includes('?') ? '&' : '?';
-      window.location.href =
-        window.location.href + separator + "refreshed=true";
-    }
-  }, []);
-
   return (
     <>
       <IndexNavbar />
